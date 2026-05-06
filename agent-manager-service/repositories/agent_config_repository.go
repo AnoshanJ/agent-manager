500527a105a467eaa0a5f0d53579c6e24c40a097
Anoshan Jayahanthan <101160077+AnoshanJ@users.noreply.github.com>
2026-05-06 17:24:00 +0530

    Add enable_api_key_security to AgentConfig Upsert DoUpdates
    Signed-off-by: Anoshan Jayahanthan <101160077+AnoshanJ@users.noreply.github.com>

--- agent-manager-service/repositories/agent_config_repository.go
+++ agent-manager-service/repositories/agent_config_repository.go
@@ -58,6 +58,7 @@ func (r *AgentConfigRepo) Upsert(config *models.AgentConfig) error {
 		Columns: []clause.Column{{Name: "org_name"}, {Name: "project_name"}, {Name: "agent_name"}, {Name: "environment_name"}},
 		DoUpdates: clause.Assignments(map[string]interface{}{
 			"enable_auto_instrumentation": config.EnableAutoInstrumentation,
+			"enable_api_key_security":     config.EnableApiKeySecurity,
 			"updated_at":                  clause.Expr{SQL: "NOW()"},
 		}),
 	}).Create(config).Error
