a81c75b5574f2be765a7ea07a5f26459a27adc6d
Ravindu <ravinduhiran26@gmail.com>
2026-07-24 10:19:42 +0530

    Change the api specifications and add the Resilience fields for the models

--- agent-manager-service/db_migrations/migration_list.go
+++ agent-manager-service/db_migrations/migration_list.go
@@ -16,7 +16,7 @@
 
 package dbmigrations
 
-const latestVersion = 39
+const latestVersion = 40
 
 // migration list sorted by version.  Add new migrations to the end of the list.
 // Previous migrations should not be modified.
@@ -60,4 +60,5 @@ var migrations = []migration{
 	migration037,
 	migration038,
 	migration039,
+	migration040,
 }
